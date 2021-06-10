b00f56a4ebe4a0f0f37192902d43e9e71cca6270
Daniel Sahara <d.otasahara@gmail.com>
2021-06-09 22:22:19 -0300

    componentização

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,7 @@
-import {soma} from './soma';
+import React from 'react';
+import { render } from 'react-dom';
+import App from './App';
 
-console.log(soma(1, 1));
+//JSX : Html dentro do Javascript
+
+render(<App />, document.getElementById('app'));
