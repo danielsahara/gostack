1a6d5a79224c1c8091a8ce4db6c4c7f7cd60e299
Daniel Sahara <d.otasahara@gmail.com>
2021-06-14 13:30:48 -0300

    tipando objetos e valores

--- typescript/src/routes.ts
+++ typescript/src/routes.ts
@@ -1,5 +1,15 @@
 import { Request, Response } from 'express';
+import createUser from './services/CreateUser';
 
 export function helloWorld(request: Request, response: Response){
+    const user = createUser({
+        email: 'diego@rocket.com',
+        password: '123456',
+        techs: ['node', 'react', {title: 'java', experience: 100}]//string[]
+    });
+
+    console.log(user);
+    
+
     return response.json({message: 'Hello'});
 }
