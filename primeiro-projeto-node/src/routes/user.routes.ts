aa509c341608026d5c4d02ea3460d8f2e6fb0a01
Daniel Sahara <d.otasahara@gmail.com>
2021-06-29 22:33:11 -0300

    upload de arquivos

--- primeiro-projeto-node/src/routes/user.routes.ts
+++ primeiro-projeto-node/src/routes/user.routes.ts
@@ -1,7 +1,11 @@
 import {Router} from 'express';
 import CreateUserService from "../services/CreateUserService";
+import ensureAuthenticated from "../middlewares/ensureAuthenticated";
+import uploadConfig from '../config/upload';
+import multer from "multer";
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
     try{
@@ -22,6 +26,14 @@ usersRouter.post('/', async (request, response) => {
     catch(err) {
         return response.status(400).json({ error: err.message });
     }
+});
+
+usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'),
+    async (request, response) => {
+
+        console.log(request.file);
+
+    return response.json({ ok: true });
 });
 
 export default usersRouter;
