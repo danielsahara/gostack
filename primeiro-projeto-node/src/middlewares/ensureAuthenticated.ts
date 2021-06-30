863be0762dbf16efccff32b6be24feb2aac415bd
Daniel Sahara <d.otasahara@gmail.com>
2021-06-29 23:06:34 -0300

    criando classe de erro

--- primeiro-projeto-node/src/middlewares/ensureAuthenticated.ts
+++ primeiro-projeto-node/src/middlewares/ensureAuthenticated.ts
@@ -1,6 +1,7 @@
 import {Request, Response, NextFunction} from 'express';
 import {verify} from "jsonwebtoken";
 import authConfig from '../config/auth';
+import AppError from '../errors/AppError';
 
 interface TokenPayload {
     iat: number;
@@ -13,7 +14,7 @@ export default function ensureAuthenticated (request: Request, response: Respons
     const authHeader = request.headers.authorization;
 
     if (!authHeader){
-        throw  new Error('JTM token is missing');
+        throw  new AppError('JTM token is missing', 401);
     }
 
     const [, token] = authHeader.split(' ');
@@ -32,7 +33,7 @@ export default function ensureAuthenticated (request: Request, response: Respons
         return next();
     }
     catch {
-        throw new Error('Invalid jwt token');
+        throw new AppError('Invalid jwt token', 401);
     }
 
 }
